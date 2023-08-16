d84acd562d181c7d379eb17dfdf3dd18569e4945
Stepa075 <stepa0751@gmail.com>
2023-08-16 19:23:45 +0300

    less0n 24, all is good!

--- app/src/main/java/com/stepa0751/alertbuttonend/location/LocationService.kt
+++ app/src/main/java/com/stepa0751/alertbuttonend/location/LocationService.kt
@@ -1,27 +1,33 @@
 package com.stepa0751.alertbuttonend.location
 
-import android.annotation.SuppressLint
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import com.google.android.gms.location.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.LocationServices.getFusedLocationProviderClient
-
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 import com.stepa0751.alertbuttonend.MainActivity
 import com.stepa0751.alertbuttonend.R
 
 //  Создали сервис для работы в фоновом режиме... В манифесте его нужно прописать!!!
 class LocationService : Service() {
 //    Эта переменная нужна для того, чтобы подключаться к провайдеру GPS и получать у него данные о местоположении
-//  И она нихрена без параметров не регистрируется в классе!!!
-    @SuppressLint("VisibleForTests")
-   private var locProvider = getFusedLocationProviderClient(baseContext)
+
+    private lateinit var locProvider: FusedLocationProviderClient
+    private lateinit var locRequest: LocationRequest
     override fun onBind(p0: Intent?): IBinder? {
         return null
     }
@@ -29,6 +35,7 @@ class LocationService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         startNotification()
+        startLocationUpdates()
         isRunning = true
         return START_STICKY
     }
@@ -40,9 +47,14 @@ class LocationService : Service() {
 
     override fun onDestroy() {
         super.onDestroy()
+//  Переменную "работает" делаем ложь
         isRunning = false
+//  Отписываеся от обновлений местоположения
+        locProvider.removeLocationUpdates(locCallBack)
     }
-//   Все что ниже - темный лес, но нужен он для запуска сервиса и отображения его в фореграунд.
+
+
+    //   Все что ниже - темный лес, но нужен он для запуска сервиса и отображения его в фореграунд.
     private fun startNotification() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val nChannel = NotificationChannel(
@@ -69,13 +81,48 @@ class LocationService : Service() {
         startForeground(99, notification)
     }
 
-
+    //  Инициализация клиента доступа к подписке на местоположение
     private fun initLocation(){
+        //   Создаем объект
+        locRequest =  LocationRequest.create()
+        //  Назначаем интервал обновления
+        locRequest.interval = 5000
+        //  Максимальная скорость обновления местоположения
+        locRequest.fastestInterval = 5000
+        //  Назначаем приоритет "Высокая точность"
+        locRequest.priority = PRIORITY_HIGH_ACCURACY
+        //  Инициализируем сам locProvider
         locProvider = LocationServices.getFusedLocationProviderClient(baseContext)
+
     }
 
+    // Сюда приходит информация о местоположении в lResult
+    private val locCallBack = object : LocationCallback(){
+        override fun onLocationResult(lResult: LocationResult) {
+            super.onLocationResult(lResult)
+            Log.d("MyLog", " Location: ${lResult.lastLocation?.latitude} : ${lResult.lastLocation?.longitude}")
+        }
+    }
+
+    //  Функция запуска слушателя местоположения, для нее нужны несколько параметров:
+    // И с начала в ней идет проверка на разрешение пользователем приложению доступа к местоположению
+    //  Так просит котлин, потому что он не понимает что мы уже где-то раньше проверяли это.
     private fun startLocationUpdates(){
-//        locProvider.requestLocationUpdates()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) return
+
+        locProvider.requestLocationUpdates(
+            //  Этот параметр получаем в initLocation
+            locRequest,
+            // Сюда будет приходить информация о нашем местоположении
+            locCallBack,
+            //  И лупер нужен, чтобы повторять поток запроса инфы о местоположении,
+            // т.к. поток закрывается после выполнения всех команд в нем.
+            Looper.myLooper()
+        )
     }
 
     companion object {
